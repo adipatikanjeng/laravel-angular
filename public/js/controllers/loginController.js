615cff8d1043b15bd2f4a36a4228cc84e33f9c86
Heriyadi <adipatikanjeng@gmail.com>
2014-08-18 15:08:56 +0700

    last update

--- public/js/controllers/loginController.js
+++ public/js/controllers/loginController.js
@@ -1,11 +1,14 @@
 var login = angular.module('LoginCtrl', []);
 
-login.controller('LoginController', function($scope, Login) {
+login.controller('LoginController', function($scope, $location, Login, SessionService) {
 
 	$scope.loginSubmit = function() {
 		var auth = Login.auth($scope.loginData);
 		auth.success(function(response) {
-			console.log(response);
+			if (response.id) {
+				alert('true');
+				SessionService.set('auth', true); //This sets our session key/val pair as authenticated
+			} else alert('could not verify your login');
 		});
 	}
 });
