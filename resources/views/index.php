aa9e63f78a50db9a3a8352376d75f9cdfd7c773c
Heriyadi <adipatikanjeng@gmail.com>
2014-11-07 19:41:07 +0700

    add bower_component

--- resources/views/index.php
+++ resources/views/index.php
@@ -77,14 +77,15 @@
 		<div id="view" ng-view></div>
 	</div>
 
-	<script src="js/angular.min.js"></script> 	
-	<script src="js/angular-route.min.js"></script>  	
+	<script src="js/angular.min.js"></script> 
+	<script src="bower_components/angular/angular.min.js"></script>		
 	<script src="js/underscore.js"></script>
 	<script src="js/app.js"></script>
 	<script src="js/controllers.js"></script>
 	<script src="js/directives.js"></script>
 	<script src="js/services.js"></script>
 	<script src="js/angular-ui-router.js"></script>	
+	<script src="bower_components/angular-route/angular-route.min.js"></script>
 	<script src="bower_components/angular-animate/angular-animate.min.js"></script>
 	<script src="bower_components/angular-sanitize/angular-sanitize.min.js"></script>
 	<script src="bower_components/toaster/toaster.js"></script>
